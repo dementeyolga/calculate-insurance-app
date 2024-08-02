1e3c6909be37a505c3c12bc2b12859f4890b4206
dementeyolga <dementeyolya@gmail.com>
2024-08-02 14:27:22 +0300

    feat: upload build

--- src/features/calculator-form/index.ts
+++
@@ -1,1 +0,0 @@
-export { CalculatorForm } from './ui/CalculatorForm';
--- src/shared/ui/input/index.ts
+++
@@ -1,4 +0,0 @@
-export { TextInput } from './ui/TextInput';
-export { RadioIcons } from './ui/RadioIcons';
-export { Checkbox } from './ui/Checkbox';
-export { Select } from './ui/Select';
